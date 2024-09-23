1f0505059d9e67c94ea682619b5314bc18b17049
Reaz2020 <reaz.morshed@yahoo.com>
2024-09-23 16:10:00 +0100

    added funtionality to donate now buttons and changed color between history and donation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 
 //-----------------
-//donation handling
+//donation handling for first card 
 document.getElementById('donate-now-btn').addEventListener('click', function() {
   
 
@@ -23,18 +23,97 @@ document.getElementById('donate-now-btn').addEventListener('click', function() {
     reduceFromInitialAmount(userAmount,newInitialSuggestedAmount,  idOfInitialSuggestedAmount)
     addAmountToCardAmount(userAmount,idOfInitialSuggestedAmountInsideCard , newInitialSuggestedAmountInsideCard )
     document.getElementById('valid-modal').classList.remove('hidden') 
+    document.getElementById('donate-now-btn').setAttribute('href', '#valid-modal');
+    creatingADivForHistoryPage(userAmount)
+    return;
+  }
+  if(!validateAmount(userAmount))
+    
+     {
+     alert('enter valid amount')
+    document.getElementById('donate-now-btn').setAttribute('href', '#');
+     
+    return;
+    
+  }
+
+ 
+})
+
+
+//----------------
+
+//donation handling for seconde card 
+document.getElementById('donate-now-btnTwo').addEventListener('click', function() {
+  
+
+  let newInitialSuggestedAmount =  parseFloat(document.getElementById('initial-suggestedAmount').innerText);
+  let newInitialSuggestedAmountInsideCard =parseFloat(document.getElementById('initial-suggestedAmountInsideCardTwo').innerText);
+
+
+ const userAmount= document.getElementById('amount-two').value;
+ 
+ parseFloat(userAmount)
+  if(validateAmount(userAmount)){
+
+
+    let idOfInitialSuggestedAmount =document.querySelector('#initial-suggestedAmount');
+    let idOfInitialSuggestedAmountInsideCard =document.querySelector('#initial-suggestedAmountInsideCardTwo');
+   
+    reduceFromInitialAmount(userAmount,newInitialSuggestedAmount,  idOfInitialSuggestedAmount)
+    addAmountToCardAmount(userAmount,idOfInitialSuggestedAmountInsideCard , newInitialSuggestedAmountInsideCard )
+    document.getElementById('valid-modal').classList.remove('hidden') 
+    document.getElementById('donate-now-btn').setAttribute('href', '#valid-modal');
+    creatingADivForHistoryPage(userAmount)
   }
   else{
     alert('enter valid amount')
+    document.getElementById('donate-now-btnTwo').setAttribute('href', '#');
     return;
     
   }
 })
-function reAddingHiddenClass(){
-  document.getElementById('valid-modal').classList.add('hidden') 
-}
+
 
 //----------------
+
+
+//donation handling for third card 
+document.getElementById('donate-now-btnThree').addEventListener('click', function() {
+  
+
+  let newInitialSuggestedAmount =  parseFloat(document.getElementById('initial-suggestedAmount').innerText);
+  let newInitialSuggestedAmountInsideCard =parseFloat(document.getElementById('initial-suggestedAmountInsideCardThree').innerText);
+
+
+ const userAmount= document.getElementById('amount-three').value;
+ 
+ parseFloat(userAmount)
+  if(validateAmount(userAmount)){
+
+
+    let idOfInitialSuggestedAmount =document.querySelector('#initial-suggestedAmount');
+    let idOfInitialSuggestedAmountInsideCard =document.querySelector('#initial-suggestedAmountInsideCardThree');
+   
+    reduceFromInitialAmount(userAmount,newInitialSuggestedAmount,  idOfInitialSuggestedAmount)
+    addAmountToCardAmount(userAmount,idOfInitialSuggestedAmountInsideCard , newInitialSuggestedAmountInsideCard )
+    document.getElementById('valid-modal').classList.remove('hidden') 
+    document.getElementById('donate-now-btnThree').setAttribute('href', '#valid-modal');
+    creatingADivForHistoryPage(userAmount)
+  }
+  else{
+    alert('enter valid amount')
+    document.getElementById('donate-now-btnThree').setAttribute('href', '#');
+    return;
+    
+  }
+})
+
+
+//------------------
+
+
+
 function reduceFromInitialAmount(amountToBeReduced,newInitialSuggestedAmount,idOfInitialSuggestedAmount) {
 
   let n=parseFloat(amountToBeReduced)
@@ -66,17 +145,19 @@ function addAmountToCardAmount(amountToBeAdded,idOfInitialSuggestedAmountInsideC
 
 //---------------------------------------------------
 
-//----- helper function
+//----- helper function for donation 
 function validateAmount(paraAmount) {
   const regex = /^(0|[1-9]\d*)(\.\d+)?$/; // Regex for non-negative numbers including floats
 
-  // Check if the amount matches the regex
+  //  if the amount matches the regex
   if (!regex.test(paraAmount)) {  
       return false;
   }
 
-  // Check if it's a valid number and greater than zero
-  if (paraAmount > 0) {
+  const parsedAmount = parseFloat(paraAmount);
+
+  //  if it's a valid number and greater than zero
+  if (parsedAmount > 0) {
       return true; // Valid positive number
   }
 
@@ -91,8 +172,72 @@ function getInputFieldValue(){
 
 
 }
-
+// when i will press to close the modal this function will re add the hidden class to modal to keep it hidden
+function reAddingHiddenClass(){ 
+  document.getElementById('valid-modal').classList.add('hidden') 
+}
 //------------------------------
 //history and donation toggling 
-document.getElementById()
 
+
+
+document.getElementById('history-btn').addEventListener('click', function() {
+
+
+  document.getElementById('history').classList.remove('hidden');
+  document.getElementById('cards').classList.add('hidden');
+
+
+  document.getElementById('history-btn').classList.remove('bg-white');
+  document.getElementById('history-btn').classList.add('bg-lime-500');
+  document.getElementById('donation-btn').classList.remove('bg-lime-500');
+
+})
+
+document.getElementById('donation-btn').addEventListener('click', function() {
+
+
+  document.getElementById('cards').classList.remove('hidden');
+  document.getElementById('history').classList.add('hidden');
+
+
+  document.getElementById('history-btn').classList.remove('bg-lime-500');
+  document.getElementById('donation-btn').classList.add('bg-lime-500');
+
+
+ // historyDonationToggling(historyId,donationId,donationBtn,historyBtn)
+
+})
+
+
+// function historyDonationToggling(historyId,donationId,donationBtn,historyBtn){
+  
+//   donationId.classList.remove('hidden');
+//   donationBtn.classList.add('bg-lime-500');
+//   historyBtn.classList.remove('bg-lime-500');
+
+// }
+
+
+function creatingADivForHistoryPage(ParaAmount){
+  const date = new Date();
+  let amount=ParaAmount;
+
+  const div = document.createElement('div');
+  div.className = 'div bg-white border border-gray-300 p-5 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 cursor-pointer';
+
+  const h1 = document.createElement('h1');
+  h1.className = 'text-xl font-semibold';
+  h1.textContent = 'Thanks for donating ' + amount;
+  div.appendChild(h1);
+
+  const p = document.createElement('p');
+  p.className = 'text-x font-semibold';
+  p.textContent = date;
+  div.appendChild(p);
+
+
+  const historySection = document.getElementById('history');
+  historySection.appendChild(div);
+
+}
